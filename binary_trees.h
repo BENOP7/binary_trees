717142afa5b5177292094c0d3791cfc4b47edd88
benop7 <benjaminpetegnong@gmail.com>
2022-08-23 14:01:01 +0100

    task 10

--- binary_trees.h
+++ binary_trees.h
@@ -28,5 +28,6 @@ void binary_tree_delete(binary_tree_t *);
 int binary_tree_is_leaf(const binary_tree_t *);
 int binary_tree_is_root(const binary_tree_t *);
 size_t binary_tree_height(const binary_tree_t *);
+size_t binary_tree_depth(const binary_tree_t *);
 
 #endif /* _BINARY_TREES_H_ */
