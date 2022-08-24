74b04a282308ecc4139453dba01a3e0057af5512
benop7 <benjaminpetegnong@gmail.com>
2022-08-24 10:28:54 +0100

    correct betty errors

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - insert a right child node
+ * @parent: parent of the new node
+ * @value: value of the inserted node
+ *
+ * Return: pointer to the inserted node
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -9,7 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
@@ -25,6 +32,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = new_node;
 		parent->right = new_node;
 	}
-	
+
 	return (new_node);
 }
