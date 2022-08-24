52e120593dbc8da1a0915c882edd5462af1b590b
benop7 <benjaminpetegnong@gmail.com>
2022-08-24 10:25:40 +0100

    correct betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - insert a left child node
+ * @parent: parent of the new node
+ * @value: value of the added child node
+ *
+ * Return: pointer to the inserted node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -9,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->right = NULL;
@@ -25,6 +32,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = new_node;
 		parent->left = new_node;
 	}
-	
+
 	return (new_node);
 }
