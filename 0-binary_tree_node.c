41d35b920a5f847a7de4b2591e4ccd3ea9c67b74
benop7 <benjaminpetegnong@gmail.com>
2022-08-24 10:19:30 +0100

    correct betty errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,23 +1,20 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+/**
+ * binary_tree_node - create a new node in the binary tree
+ * @parent: the parent node of the new node
+ * @value: the value of the node
+ * Return: a pointer to the new node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	
+
 	new = malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
 	new->n = value;
-	/*new->parent = malloc(sizeof(new->parent));
-	if (!new->parent)
-		return (NULL);
-	new->parent = parent;
-	new->left = malloc(sizeof(*(new->left)));
-	if (!new->left)
-		return (NULL);
-	new->right = malloc(sizeof(*(new->right)));
-		return (NULL);*/
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
