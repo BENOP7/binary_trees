4ea0fb393873b288cc588c624b51826439847595
benop7 <benjaminpetegnong@gmail.com>
2022-08-24 10:42:00 +0100

    correct argument type

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
-int binary_tree_is_leaf(binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node)
 	{
 		if (node->left == NULL && node->right == NULL)
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
