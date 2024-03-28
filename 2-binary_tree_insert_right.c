b73699b5ac22e2e201ed70afda4d1e1eb432c6a5
benop7 <benjaminpetegnong@gmail.com>
2024-03-28 03:38:54 +0100

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,27 +2,27 @@
 
 
 /**
- * binary_tree_insert_left - creates a new node and inserts it as the left
+ * binary_tree_insert_right - creates a new node and inserts it as the left
  * child of the parent node passed as argument
  * @parent: the parent node of the node to be created
  * @value: the value to be assigned to the node
  *
  * Return: a pointer to the new node otherwise return NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 	binary_tree_t *tmp = NULL;
 
 	if (parent)
 	{
-		tmp = parent->left;
+		tmp = parent->right;
 		new = binary_tree_node(parent, value);
 		if (tmp)
 			tmp->parent = new;
-		new->left = tmp;
-		new->right = NULL;
-		parent->left = new;
+		new->left = NULL;
+		new->right = tmp;
+		parent->right = new;
 
 	}
 
